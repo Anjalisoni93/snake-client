23027c82151681aaa8deb92fd963823cb5f4c827
Anjali Soni <anjalipatadiya@gmail.com>
2021-11-17 18:02:03 -0700

    Played with move commands to undesrtand the logic

--- client.js
+++ client.js
@@ -11,19 +11,31 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write('Name: AJS');
-  });
 
+    /* setTimeout(() => {
+      conn.write('Move: up')
+    }, 50);
+    conn.write('Move: down');
+    conn.write('Move: left');
+    conn.write('Move: right');
+    
+    setInterval(() => {
+      conn.write('Move: up')
+    }, 50);
+
+    setTimeout(() => {
+      conn.write("Move: up")
+    }, 50); */
+
+  });
 
   // Added an event data which gets complete data and displays a message
   conn.on("data", (data) => {
     console.log('Server says..', data);
   });
 
-  
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
   
   return conn;
 };
