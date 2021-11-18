234e1ae3c845317ac1771583890e4842f8d0c207
Anjali Soni <anjalipatadiya@gmail.com>
2021-11-17 18:42:15 -0700

    Added two functions and some comments to this file

--- play.js
+++ play.js
@@ -9,17 +9,19 @@ const setupInput = function () {
   stdin.setEncoding("utf8");
   stdin.resume();
 
+  //added an eventlistener
   stdin.on("data", handleUserInput);
 
   return stdin;
 };
 
+// stuff happens here when particular key is pressed
 const handleUserInput = function (key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
   }
-};
+};s
 
 // establishing connection
 console.log("Connecting ...");
