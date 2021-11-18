cfccc343ae8b44d0ce8bbb6f1c7b9249a7357508
Anjali Soni <anjalipatadiya@gmail.com>
2021-11-17 18:55:10 -0700

    Removed two functions and moved to input.js and added require to update the file.

--- play.js
+++ play.js
@@ -1,27 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 //const net = require("net");
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  //added an eventlistener
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-// stuff happens here when particular key is pressed
-const handleUserInput = function (key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};s
 
 // establishing connection
 console.log("Connecting ...");
