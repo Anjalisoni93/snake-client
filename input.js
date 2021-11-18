ac1c2d212c4bf396d84df2231ec895ffe89ec521
Anjali Soni <anjalipatadiya@gmail.com>
2021-11-17 20:46:40 -0700

    Added W S A D and two more conditions and added connection.

--- input.js
+++ input.js
@@ -1,5 +1,11 @@
+// const { connect } = require("./client");
+
+
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -16,6 +22,24 @@ const handleUserInput = function(key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 'q') {
+    connection.write('Say: Oops!');
+  }
+  if (key === 'e') {
+    connection.write('Say: HahaNo!');
   }
 };
 
